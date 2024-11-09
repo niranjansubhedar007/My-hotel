fb25b08385d87a1e660f2eb8c77c8e924c5d0296
Niranjan Subhedar <niranjansubhedar@gmail.com>
2024-11-09 11:21:33 +0530

    uninstall micromatch

--- next.config.js
+++ next.config.js
@@ -1,28 +1,17 @@
-// /** @type {import('next').NextConfig} */
-// // const nextConfig = {}
-
-// // module.exports = nextConfig
-
-
-// const nextConfig = {
-//     images: {
-//       domains: ['192.168.1.9'], // Add your 192.168.1.9 hostname here
-//     },
-//     server: {
-//       host: '0.0.0.0',
-//       port: 3000, // or your preferred port
-//     },
-//   }
-  
-//   module.exports = nextConfig
-
-
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-      domains: ['192.168.1.9'], // Add your 192.168.1.9 hostname here for external images
-  },
-};
+// const nextConfig = {}
 
-module.exports = nextConfig;
+// module.exports = nextConfig
+
+
+const nextConfig = {
+    images: {
+      domains: ['192.168.1.9'], // Add your 192.168.1.9 hostname here
+    },
+    server: {
+      host: '0.0.0.0',
+      port: 3000, // or your preferred port
+    },
+  }
+  
+  module.exports = nextConfig
